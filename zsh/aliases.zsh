7eaf72cb2731c88acbcf18ae6d17236978d692fa
Richard Ashworth <richardashworth575@gmail.com>
2017-01-22 22:48:14 +0000

    commented ruby-specific aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -146,13 +146,13 @@ alias cl='clear'
 alias gz='tar -zcvf'
 
 # Ruby
-alias c='rails c' # Rails 3
-alias co='script/console' # Rails 2
-alias cod='script/console --debugger'
+# alias c='rails c' # Rails 3
+# alias co='script/console' # Rails 2
+# alias cod='script/console --debugger'
 
 #If you want your thin to listen on a port for local VM development
 #export VM_IP=10.0.0.1 <-- your vm ip
-alias ts='thin start -a ${VM_IP:-127.0.0.1}'
+  lias ts='thin start -a ${VM_IP:-127.0.0.1}'
 alias ms='mongrel_rails start'
 alias tfdl='tail -f log/development.log'
 alias tftl='tail -f log/test.log'
@@ -166,7 +166,7 @@ alias sgi='sudo gem install --no-ri --no-rdoc'
 # TODOS
 # This uses NValt (NotationalVelocity alt fork) - http://brettterpstra.com/project/nvalt/
 # to find the note called 'todo'
-alias todo='open nvalt://find/todo'
+# alias todo='open nvalt://find/todo'
 
 # Forward port 80 to 3000
 alias portforward='sudo ipfw add 1000 forward 127.0.0.1,3000 ip from any to any 80 in'
@@ -175,23 +175,23 @@ alias rdm='rake db:migrate'
 alias rdmr='rake db:migrate:redo'
 
 # Zeus
-alias zs='zeus server'
-alias zc='zeus console'
-alias zr='zeus rspec'
-alias zrc='zeus rails c'
-alias zrs='zeus rails s'
-alias zrdbm='zeus rake db:migrate'
-alias zrdbtp='zeus rake db:test:prepare'
-alias zzz='rm .zeus.sock; pkill zeus; zeus start'
+# alias zs='zeus server'
+# alias zc='zeus console'
+# alias zr='zeus rspec'
+# alias zrc='zeus rails c'
+# alias zrs='zeus rails s'
+# alias zrdbm='zeus rake db:migrate'
+# alias zrdbtp='zeus rake db:test:prepare'
+# alias zzz='rm .zeus.sock; pkill zeus; zeus start'
 
 # Rspec
-alias rs='rspec spec'
-alias sr='spring rspec'
-alias src='spring rails c'
-alias srgm='spring rails g migration'
-alias srdm='spring rake db:migrate'
-alias srdt='spring rake db:migrate'
-alias srdmt='spring rake db:migrate db:test:prepare'
+# alias rs='rspec spec'
+# alias sr='spring rspec'
+# alias src='spring rails c'
+# alias srgm='spring rails g migration'
+# alias srdm='spring rake db:migrate'
+# alias srdt='spring rake db:migrate'
+# alias srdmt='spring rake db:migrate db:test:prepare'
 
 
 # Sprintly - https://github.com/nextbigsoundinc/Sprintly-GitHub
