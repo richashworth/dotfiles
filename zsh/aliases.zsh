e0d4bb7dd0166e430af4249738acadd3efc4e2d8
Richard Ashworth <richardashworth575@gmail.com>
2017-05-30 09:56:03 +0100

    za

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -210,7 +210,8 @@ alias aup='bup;cup'
 # Vim
 alias vi='vim'
 alias v='vim'
-alias wiki='vim ~/vimwiki/index.wiki -c "set background=light spell"'
+alias wikil='vim ~/vimwiki/index.wiki -c "set background=light spell"'
+alias wiki='vim ~/vimwiki/index.wiki -c "set spell"'
 alias w='wiki'
 
 # sbt
