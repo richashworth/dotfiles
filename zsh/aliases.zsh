87acfcf557d0704980738c62adb343c6c0182fe9
Richard Ashworth <richardashworth575@gmail.com>
2019-01-23 10:07:59 +0000

    aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -220,7 +220,7 @@ alias dbmu='spring rake db:migrate:up'
 
 # Homebrew
 # alias brewu='brew update  && brew upgrade && brew cleanup && brew prune && brew doctor'
-alias brewu='brew update  && brew upgrade && brew cleanup && brew prune'  # -- doctor is noisy
+alias brewu='brew update  && brew upgrade && brew cleanup'  # -- doctor is noisy
 alias brewup='brewu'
 alias bup='brewu'
 alias cup='brew cleanup'
